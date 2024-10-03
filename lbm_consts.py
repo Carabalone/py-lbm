f052664c6b6d8d4d666710ffc34710f5a22a1639
Carabalone <guilherme.caraba@gmail.com>
2024-10-03 09:55:02 +0100

    added MRT moment space and relxation matrix

--- lbm_consts.py
+++ lbm_consts.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv, diag
 # each lattice will have this structure with discrete velocities:
 #  
 #   6  2  5   
@@ -35,7 +36,32 @@ opposite_indices = np.array([
   0, 3, 4, 1, 2, 7, 8, 5, 6
 ])
 
-# ---------------------- Boundar Conditions ------------------------
+# ---------------------- MRT ---------------------------
+
+# Gram-Schmidt Transformation Matrix
+# Lattice Boltzmann method principles and practice pg. 420 eq 10.30
+M = np.array([
+    [ 1,  1,  1,  1,  1,  1,  1,  1,  1], # ρ
+    [-4, -1, -1, -1, -1,  2,  2,  2,  2], # e
+    [ 4, -2, -2, -2, -2,  1,  1,  1,  1], # e
+    [ 0,  1,  0, -1,  0,  1, -1, -1,  1], # jx
+    [ 0, -2,  0,  2,  0,  1, -1, -1,  1], # qx
+    [ 0,  0,  1,  0, -1,  1,  1, -1, -1], # jy
+    [ 0,  0, -2,  0,  2,  1,  1, -1, -1], # qy
+    [ 0,  1, -1,  1, -1,  0,  0,  0,  0], # pxx
+    [ 0,  0,  0,  0,  0,  1, -1,  1, -1], # pyy
+])
+
+M_inverse = np.linalg.inv(M)
+
+omega_e   = 1.0    # ωₑ
+omega_eps = 1.0    # ω_ε
+omega_q   = 1.0    # ω_q
+omega_nu  = 1/0.55    # ω_ν (1 / tau)
+
+# Gram-Schmidt Relaxation Matrix
+G = np.diag([0, omega_e, omega_eps, 0, omega_q, 0, omega_q, omega_nu, omega_nu])
+# ---------------------- Boundary Conditions ------------------------
 CYLINDER_CENTER = (NX // 4, NY // 2)
 CYLINDER_RADIUS = NY // 9 
 
